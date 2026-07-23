0ce31780fb006d846c4ea47b48154c1673a53c45
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-07-23 12:09:12 -0500

    reformat

--- cxoneflow_audit/core/auditor.py
+++ cxoneflow_audit/core/auditor.py
@@ -1,16 +1,17 @@
 from .common import Operation
 
+
 class Auditor(Operation):
 
-  def __init__(self, outfile : str, only_not_cfg : bool, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.__outfile = outfile
-    self.__only_not_cfg = only_not_cfg
+    def __init__(self, outfile: str, only_not_cfg: bool, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__outfile = outfile
+        self.__only_not_cfg = only_not_cfg
 
-  @property
-  def outfile(self) -> str:
-    return self.__outfile
-  
-  @property
-  def skip_configured(self) -> bool:
-    return self.__only_not_cfg
+    @property
+    def outfile(self) -> str:
+        return self.__outfile
+
+    @property
+    def skip_configured(self) -> bool:
+        return self.__only_not_cfg
