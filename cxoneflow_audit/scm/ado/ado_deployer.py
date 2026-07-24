50214ac335d0f5dc790e3150488309cedcbfe4b1
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-07-24 09:56:03 -0500

    format

--- cxoneflow_audit/scm/ado/ado_deployer.py
+++ cxoneflow_audit/scm/ado/ado_deployer.py
@@ -28,7 +28,8 @@ class AdoDeployer(Deployer):
                 self.scm_service.update_hook_by_event_type(event, hook_data, sub)
 
         if (
-            await self.scm_service.evaluate_subscription_state(hook_data) == ConfigState.CONFIGURED
+            await self.scm_service.evaluate_subscription_state(hook_data)
+            == ConfigState.CONFIGURED
             and not self.replace
         ):
             self.log().warning(
@@ -40,8 +41,24 @@ class AdoDeployer(Deployer):
 
         sub_ids = [s["id"] for s in subs]
         if len(sub_ids) > 0:
-            await gather(*[self.scm_service.delete_subscription(lu["collection"],sub_id) for sub_id in sub_ids])
+            await gather(
+                *[
+                    self.scm_service.delete_subscription(lu["collection"], sub_id)
+                    for sub_id in sub_ids
+                ]
+            )
 
-        await gather(*[self.scm_service.create_subscription(event,lu["id"],lu["collection"],self.cxoneflow_url,self.shared_secret) for event in ADOService.ADO_EVENT_TYPES])
+        await gather(
+            *[
+                self.scm_service.create_subscription(
+                    event,
+                    lu["id"],
+                    lu["collection"],
+                    self.cxoneflow_url,
+                    self.shared_secret,
+                )
+                for event in ADOService.ADO_EVENT_TYPES
+            ]
+        )
 
         return True
