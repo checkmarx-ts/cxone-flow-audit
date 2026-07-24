88a84499cc91a06d13660ec9419aa4eaa9c9407f
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-07-24 09:53:59 -0500

    refactor

--- cxoneflow_audit/scm/ado/ado_auditor.py
+++ cxoneflow_audit/scm/ado/ado_auditor.py
@@ -1,63 +1,64 @@
-from cxoneflow_audit.core import Auditor
-from cxoneflow_audit.core.common import ConfigState
+import csv
 from typing import Dict, AsyncGenerator, Any, Awaitable
 from asyncio import gather, Lock
-import csv
-from .ado_base import AdoBase, HookData
+from cxoneflow_audit.core import Auditor
+from cxoneflow_audit.core.common import ConfigState
+from .ado_service import HookData, ADOService
 
 
-class AdoAuditor(Auditor, AdoBase):
+class AdoAuditor(Auditor):
 
-    def __init__(self, *args, **kwargs):
-        Auditor.__init__(self, *args, **kwargs)
-        AdoBase.__init__(self)
-
-    @property
-    def _scm_name(self) -> str:
-        return "ADO"
+    def __init__(self, scm_api_service: ADOService, *args, **kwargs):
+        Auditor.__init__(self, scm_api_service=scm_api_service, *args, **kwargs)
 
     async def _evaluate_subscription_state(self, project_id: str) -> ConfigState:
         if project_id not in self.__data.keys():
             return ConfigState.NOT_CONFIGURED
 
-        return await super()._evaluate_subscription_state(self.__data[project_id])
+        return await self.scm_service.evaluate_subscription_state(
+            self.__data[project_id]
+        )
 
     async def __record_pr_create(self, project_id: str, sub_json: Dict) -> None:
         async with self.__lock:
-            self._update_hook_pr_create_from_sub_json(self.__data[project_id], sub_json)
+            self.scm_service.update_hook_pr_create_from_sub_json(
+                self.__data[project_id], sub_json
+            )
 
     async def __record_pr_update(self, project_id: str, sub_json: Dict) -> None:
         async with self.__lock:
-            self._update_hook_pr_update_from_sub_json(self.__data[project_id], sub_json)
+            self.scm_service.update_hook_pr_update_from_sub_json(
+                self.__data[project_id], sub_json
+            )
 
     async def __record_push(self, project_id: str, sub_json: Dict) -> None:
         async with self.__lock:
-            self._update_hook_push_from_sub_json(self.__data[project_id], sub_json)
+            self.scm_service.update_hook_push_from_sub_json(
+                self.__data[project_id], sub_json
+            )
 
     async def __validate_event_set(
         self, project_id: str, event_name: str, record_lambda: Awaitable, sub_json: Dict
     ) -> None:
-        for sub in self._get_subs_for_project(
+        for sub in self.scm_service.get_subs_for_project(
             sub_json,
             project_id,
-            self._make_cx_endpoint_url(self.cxone_flow_url),
+            self.scm_service.make_cx_endpoint_url(self.cxoneflow_url),
             [event_name],
         ):
             await record_lambda(project_id, sub)
 
     async def _process_lu(self, lu_data: Any) -> bool:
-        self.log().debug(f"Processing: {self._get_lu_repr(lu_data)}")
+        self.log().debug(f"Processing: {self.scm_service.get_lu_repr(lu_data)}")
 
         async with self.__lock:
             if lu_data["id"] not in self.__data.keys():
-                self.__data[lu_data["id"]] = self._hook_data_from_lu_factory(lu_data)
+                self.__data[lu_data["id"]] = self.scm_service.hook_data_from_lu_factory(
+                    lu_data
+                )
 
-        subs = await self._list_lu_webhook_subscriptions(
-            self.scm_base_url,
-            lu_data["collection"],
-            self.scm_pat,
-            self.proxies,
-            self.ignore_ssl_errors,
+        subs = await self.scm_service.list_lu_webhook_subscriptions(
+            lu_data["collection"]
         )
 
         await gather(
@@ -73,22 +74,6 @@ class AdoAuditor(Auditor, AdoBase):
         )
 
         return True
-
-    def _get_lu_name(self, lu: Any) -> str:
-        return self._render_lu_name(lu)
-
-    def _get_lu_repr(self, lu: Any) -> str:
-        return self._render_lu_repr(lu)
-
-    async def _lu_iterator(self) -> AsyncGenerator[Dict, None]:
-        async for x in self._lu_iterator_delegate(
-            self.scm_base_url,
-            self.targets,
-            self.scm_pat,
-            self.proxies,
-            self.ignore_ssl_errors,
-        ):
-            yield x
 
     async def execute(self) -> int:
         self.__data = {}
